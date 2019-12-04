4cbef2d7b726e671398a4c2effbb13a30cd7d25f
Maarten van Megen <maarten.vanmegen@nedap.com>
2019-12-04 23:33:57 +0100

    add return code to test script

--- all_tests.py
+++ all_tests.py
@@ -3,4 +3,9 @@ import unittest
 if __name__ == '__main__':
     allTests = unittest.TestLoader().discover('src/test/tests_2019')
 
-    unittest.TextTestRunner(verbosity=2).run(allTests)
+    result = unittest.TextTestRunner(verbosity=2).run(allTests)
+
+    if result.wasSuccessful():
+        exit(0)
+    else:
+        exit(1)
