821607f2f87b7370fdb7738bf228db811fbb33ba
Maarten van Megen <maarten.vanmegen@nedap.com>
2019-12-05 09:45:31 +0100

    prepare for day5

--- src/utility/OpCodeRunner.py
+++ src/utility/OpCodeRunner.py
@@ -1,20 +1,40 @@
 from enum import Enum
 
 
+# TODO make instruction respect direct/indirect mode
+
+
 class Opcode(Enum):
-    # arithmetic
     ADD = 1
     MULTIPLY = 2
-    INCREMENT = 3
-    DECREMENT = 4
-    SUBTRACT = 5
-    DIVIDE = 6
-    # Jumps
-    JUMP = 7
-    # Data transfer
-    MOVE = 8
-    # Control
+    INPUT = 3
+    OUTPUT = 4
     HALT = 99
+
+
+class ParameterMode(Enum):
+    POSITION = 0
+    IMMEDIATE = 1
+
+
+class Instruction:
+
+    def __init__(self, opcode, mode_1, mode_2, mode_3):
+        self.opcode = opcode
+        self.mode_1 = mode_1
+        self.mode_2 = mode_2
+        self.mode_3 = mode_3
+
+    @staticmethod
+    def parse(int_code):
+        string_code = str(int_code)
+        length = len(string_code)
+        if length < 5:
+            # add zero padding
+            padding = "".join(["0" for _ in range(5 - length)])
+            string_code = padding + string_code
+        return Instruction(Opcode(int(string_code[3:5])), ParameterMode(int(string_code[2])),
+                           ParameterMode(int(string_code[1])), ParameterMode(int(string_code[0])))
 
 
 class OpcodeRunner:
@@ -23,9 +43,8 @@ class OpcodeRunner:
         self.program = program
         self.runnable = program.copy()
         self.pointer = 0
-        self.opcode_to_method = {Opcode.DIVIDE: self.divide, Opcode.ADD: self.add, Opcode.MULTIPLY: self.multiply,
-                                 Opcode.JUMP: self.jump, Opcode.MOVE: self.move, Opcode.SUBTRACT: self.subtract,
-                                 Opcode.INCREMENT: self.increment, Opcode.DECREMENT: self.decrement}
+        self.opcode_to_method = {Opcode.ADD: self.add, Opcode.MULTIPLY: self.multiply, Opcode.INPUT: self.get_input,
+                                 Opcode.OUTPUT: self.output}
 
     def reset(self):
         self.runnable = self.program.copy()
@@ -33,54 +52,32 @@ class OpcodeRunner:
 
     def run_program(self):
         # run while instruction pointer does not contain halt
-        while Opcode(self.runnable[self.pointer]) != Opcode.HALT:
+        while Instruction.parse(self.runnable[self.pointer]).opcode != Opcode.HALT:
             # do action
-            opcode = Opcode(self.runnable[self.pointer])
+            opcode = Instruction.parse(self.runnable[self.pointer]).opcode
             operation = self.opcode_to_method.get(opcode, lambda: print("Unexpected opcode: {}".format(opcode)))
             operation()
 
-    def move(self):
-        # move data from location a, to location b as specified in pointers 1 and 2
-        address_1 = self.pointer + 1
-        address_2 = self.pointer + 2
+    def get_input(self, address):
+        print("Please provide program input:")
+        input_value = input()
+        self.set_value(address, input_value)
+        self.pointer += 2
 
-        value = self.load_from_addr_pointer(address_1)
-        self.set_value(address_2, value)
-        self.pointer += 3
+    def output(self, address):
+        print("Program result: {}".format(self.load_from_addr_pointer(address)))
+        self.pointer += 2
 
     def set_value(self, address_2, value):
         self.runnable[address_2] = value
-
-    def jump(self):
-        self.pointer = self.load_from_addr_pointer(self.pointer + 1)
 
     def add(self):
         addres, value1, value2 = self.get_arithmetic_data()
         self.set_value(addres, value1 + value2)
 
-    def increment(self):
-        address = self.pointer + 1
-        value = self.load_from_addr_pointer(address)
-        self.set_value(address, value + 1)
-        self.pointer += 2
-
-    def decrement(self):
-        address = self.pointer + 1
-        value = self.load_from_addr_pointer(address)
-        self.set_value(address, value - 1)
-        self.pointer += 2
-
-    def subtract(self):
-        addres, value1, value2 = self.get_arithmetic_data()
-        self.set_value(addres, value1 - value2)
-
     def multiply(self):
         addres, value1, value2 = self.get_arithmetic_data()
         self.set_value(addres, value1 * value2)
-
-    def divide(self):
-        addres, value1, value2 = self.get_arithmetic_data()
-        self.set_value(addres, value1 / value2)
 
     def get_arithmetic_data(self):
         value1 = self.load_from_addr_pointer(self.pointer + 1)
