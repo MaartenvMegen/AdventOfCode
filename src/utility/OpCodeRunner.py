695fa77040e8310b6cf245e69d441171f7fa9aaa
Maarten van Megen <maarten.vanmegen@nedap.com>
2019-12-05 21:41:26 +0100

    Eliminate duplicated code

--- src/utility/OpCodeRunner.py
+++ src/utility/OpCodeRunner.py
@@ -31,13 +31,8 @@ class Instruction:
 
     @staticmethod
     def parse(int_code):
-        string_code = str(int_code)
-        length = len(string_code)
-        if length < 5:
-            # add zero padding
-            padding = "".join(["0" for _ in range(5 - length)])
-            string_code = padding + string_code
-        return Instruction(Opcode(int(string_code[3:5])), ParameterMode(int(string_code[2])),
+        string_code = str(int_code).zfill(5)
+        return Instruction(Opcode(int(string_code[-2:])), ParameterMode(int(string_code[2])),
                            ParameterMode(int(string_code[1])), ParameterMode(int(string_code[0])))
 
 
@@ -45,7 +40,7 @@ class OpcodeRunner:
 
     def __init__(self, program, inputs=None):
         self.program = program
-        self.runnable = program.copy()
+        self.memory = program.copy()
         self.pointer = 0
         self.opcode_to_method = {Opcode.ADD: self.add, Opcode.MULTIPLY: self.multiply, Opcode.INPUT: self.get_input,
                                  Opcode.OUTPUT: self.output, Opcode.JUMP_IF_TRUE: self.jump_if_true,
@@ -58,24 +53,22 @@ class OpcodeRunner:
         self.outputs = []
 
     def reset(self):
-        self.runnable = self.program.copy()
+        self.memory = self.program.copy()
         self.pointer = 0
 
     def run_program(self):
-        # run while instruction pointer does not contain halt
-        while Instruction.parse(self.runnable[self.pointer]).opcode != Opcode.HALT:
-            # do action
-            instruction = Instruction.parse(self.runnable[self.pointer])
+        while Instruction.parse(self.memory[self.pointer]).opcode != Opcode.HALT:
+            instruction = Instruction.parse(self.memory[self.pointer])
             operation = self.opcode_to_method.get(instruction.opcode,
                                                   lambda: print("Unexpected opcode: {}".format(instruction.opcode)))
             operation(instruction)
 
     def set_value(self, address_2, value):
-        self.runnable[address_2] = value
+        self.memory[address_2] = value
 
     def get_input(self, instruction):
         input_value = self.inputs.pop(0)
-        address = self.runnable[self.pointer + 1]
+        address = self.memory[self.pointer + 1]
         self.set_value(address, input_value)
         self.pointer += 2
 
@@ -88,72 +81,63 @@ class OpcodeRunner:
         if mode == ParameterMode.POSITION:
             return self.load_from_addr_pointer(pointer)
         elif mode == ParameterMode.IMMEDIATE:
-            return self.runnable[pointer]
+            return self.memory[pointer]
         else:
             print("ERROR: unexpected mode")
             return None
 
     def add(self, instruction):
-        value_1 = self.get_value(self.pointer + 1, instruction.mode_1)
-        value_2 = self.get_value(self.pointer + 2, instruction.mode_2)
-        address = self.runnable[self.pointer + 3]
-
+        address, value_1, value_2 = self.get_params(instruction)
         self.set_value(address, value_1 + value_2)
-        self.pointer += 4
 
     def multiply(self, instruction):
+        address, value_1, value_2 = self.get_params(instruction)
+        self.set_value(address, value_1 * value_2)
+
+    def get_params(self, instruction):
         value_1 = self.get_value(self.pointer + 1, instruction.mode_1)
         value_2 = self.get_value(self.pointer + 2, instruction.mode_2)
-        address = self.runnable[self.pointer + 3]
-
-        self.set_value(address, value_1 * value_2)
+        address = self.memory[self.pointer + 3]
         self.pointer += 4
+        return address, value_1, value_2
 
     def jump_if_true(self, instruction):
-        value_1 = self.get_value(self.pointer + 1, instruction.mode_1)
-        if value_1 != 0:
-            self.pointer = self.get_value(self.pointer + 2, instruction.mode_2)
-        else:
-            self.pointer += 3
+        self.jump_if(instruction, lambda x: x != 0)
 
     def jump_if_false(self, instruction):
+        self.jump_if(instruction, lambda x: x == 0)
+
+    def jump_if(self, instruction, comparison):
         value_1 = self.get_value(self.pointer + 1, instruction.mode_1)
-        if value_1 == 0:
+        if comparison(value_1):
             self.pointer = self.get_value(self.pointer + 2, instruction.mode_2)
         else:
             self.pointer += 3
 
     def less_than(self, instruction):
-        value_1 = self.get_value((self.pointer + 1), instruction.mode_1)
-        value_2 = self.get_value(self.pointer + 2, instruction.mode_2)
-        address = self.runnable[self.pointer + 3]
-        if value_1 < value_2:
-            self.set_value(address, 1)
-        else:
-            self.set_value(address, 0)
-        self.pointer += 4
+        self.compare(instruction, lambda x,y: x < y)
 
     def equals(self, instruction):
-        value_1 = self.get_value((self.pointer + 1), instruction.mode_1)
-        value_2 = self.get_value(self.pointer + 2, instruction.mode_2)
-        address = self.runnable[self.pointer + 3]
-        if value_1 == value_2:
+        self.compare(instruction, lambda x, y: x == y)
+
+    def compare(self, instruction, comparison):
+        address, value_1, value_2 = self.get_params(instruction)
+        if comparison(value_1, value_2):
             self.set_value(address, 1)
         else:
             self.set_value(address, 0)
-        self.pointer += 4
 
     def load_from_addr_pointer(self, pointer):
-        return self.runnable[self.runnable[pointer]]
+        return self.memory[self.memory[pointer]]
 
     def set_pointer(self, address):
         self.pointer = address
 
     def get_result(self):
-        return self.runnable[0]
+        return self.memory[0]
 
     def get_memory(self):
-        return self.runnable
+        return self.memory
 
     def get_outputs(self):
         return self.outputs
