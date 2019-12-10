aa250063b44c835fa7af88feff3db3b6b8302596
Maarten van Megen <maarten.vanmegen@nedap.com>
2019-12-10 20:59:49 +0100

    add more test capabilities

--- all_tests.py
+++ all_tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 if __name__ == '__main__':
-    allTests = unittest.TestLoader().discover('src/test/tests_2019')
+    allTests = unittest.TestLoader().discover('src/test')
 
     result = unittest.TextTestRunner(verbosity=2).run(allTests)
 
