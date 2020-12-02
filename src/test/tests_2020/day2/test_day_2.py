f0baa7f754e7e3358febc4d7fb42c68f2b5491b5
Maarten <maarten.vanmegen@nedap.com>
2020-12-02 09:00:42 +0100

    Solution for day 2

---
+++ src/test/tests_2020/day2/test_day_2.py
@@ -0,0 +1,48 @@
+import os
+import unittest
+
+from src.utility import lineyielder
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+import re
+
+
+def get_valid_passwords_from_file(filename, validator):
+    count = 0
+    for line in lineyielder.yield_lines(os.path.join(THIS_DIR, filename)):
+        (minrep, maxrep, char, _, passwd) = re.split(' |:|-', line)
+        if validator(minrep, maxrep, char, passwd):
+            count += 1
+    return count
+
+
+def validate_toboggen(pos_a, pos_b, char, passwd):
+    if passwd[int(pos_a) - 1] == char and not passwd[int(pos_b) - 1] == char:
+        return True
+    elif passwd[int(pos_b) - 1] == char and not passwd[int(pos_a) - 1] == char:
+        return True
+    return False
+
+
+def validate_slide(minrep, maxrep, char, passwd):
+    occurence = passwd.count(char)
+    return int(minrep) <= occurence <= int(maxrep)
+
+
+class Day2Tester(unittest.TestCase):
+
+    def test_part_a_example(self):
+        count = get_valid_passwords_from_file('example1.txt', validate_slide)
+        self.assertEqual(2, count)
+
+    def test_part_a(self):
+        count = get_valid_passwords_from_file('part_a.txt', validate_slide)
+        self.assertEqual(493, count)
+
+    def test_part_b_example(self):
+        count = get_valid_passwords_from_file("example1.txt", validate_toboggen)
+        self.assertEqual(1, count)
+
+    def test_part_b(self):
+        count = get_valid_passwords_from_file("part_a.txt", validate_toboggen)
+        self.assertEqual(593, count)
