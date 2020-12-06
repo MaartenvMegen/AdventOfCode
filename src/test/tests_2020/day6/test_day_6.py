7e79ce972248f1ae7672999bf8e218ca613c7960
Maarten <maarten.vanmegen@nedap.com>
2020-12-06 15:53:53 +0100

    Day 6 - Restructured

--- src/test/tests_2020/day6/test_day_6.py
+++ src/test/tests_2020/day6/test_day_6.py
@@ -1,76 +1,42 @@
 import os
 import unittest
-
 from src.utility import lineyielder
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-import re
 
 
-def get_answers_from_file(filename):
-    answers = []
-    group_answers = set()
-    evaluation_counter = 0
-
-    for line in lineyielder.yield_lines(os.path.join(THIS_DIR, filename)):
-        if not line:
-            # store passport if valid and create a new one
-            evaluation_counter += 1
-            answers.append(group_answers)
-            group_answers = set()
-        else:
-            for char in line:
-                group_answers.add(char)
-
-    print(f'evaluated {evaluation_counter} group answers')
-    return answers
+def get_answered_by_anyone(group_questionair: str) -> set:
+    answers = [set(individual) for individual in group_questionair.split('\n') if individual]
+    return set.union(*answers) if answers else set()
 
 
-def get_answers_from_file_part_2(filename):
-    answers = []
-    group_answers = set()
-    evaluation_counter = 0
+def get_answered_by_everyone(group_questionair: str) -> set:
+    answers = [set(individual) for individual in group_questionair.split('\n') if individual]
+    return set.intersection(*answers) if answers else set()
 
-    start = True
-    for line in lineyielder.yield_lines(os.path.join(THIS_DIR, filename)):
-        if not line:
-            # store passport if valid and create a new one
-            evaluation_counter += 1
-            answers.append(group_answers)
-            print(group_answers)
-            group_answers = set()
-            start = True
-        else:
-            if start:
-                group_answers.update(set(line))
-                start = False
-            else:
-                group_answers = group_answers.intersection(set(line))
 
-    print(f'evaluated {evaluation_counter} group answers')
-    return answers
+def get_answers_from_file(filename, evaluation_method) -> list:
+    return [evaluation_method(questionair) for questionair in lineyielder.yield_chunks(filename, THIS_DIR, "\n\n")]
 
 
 class Day6Tester(unittest.TestCase):
 
     def test_part_a_example(self):
-        group_answers = get_answers_from_file('example.txt', )
+        group_answers = get_answers_from_file("example.txt", get_answered_by_anyone)
         answer = sum([len(answers) for answers in group_answers])
         self.assertEqual(11, answer)
 
-
     def test_part_a(self):
-        group_answers = get_answers_from_file('input.txt', )
+        group_answers = get_answers_from_file('input.txt', get_answered_by_anyone)
         answer = sum([len(answers) for answers in group_answers])
         self.assertEqual(6335, answer)
 
-
     def test_part_b_example(self):
-        group_answers = get_answers_from_file_part_2('example.txt', )
+        group_answers = get_answers_from_file('example.txt', get_answered_by_everyone)
         answer = sum([len(answers) for answers in group_answers])
         self.assertEqual(6, answer)
 
     def test_part_b(self):
-        group_answers = get_answers_from_file_part_2('input.txt', )
+        group_answers = get_answers_from_file('input.txt', get_answered_by_everyone)
         answer = sum([len(answers) for answers in group_answers])
         self.assertEqual(3392, answer)
--- src/utility/lineyielder.py
+++ src/utility/lineyielder.py
@@ -8,14 +8,15 @@ def yield_lines(filename: str):
             yield line.rstrip()
 
 
-def yield_lines_fp(filename_full_path: str, current_dir: str) -> str:
-    filename_full_path = os.path.join(current_dir, filename_full_path)
+def yield_lines_fp(filename: str, current_dir: str) -> str:
+    filename_full_path = os.path.join(current_dir, filename)
     with open(filename_full_path, "r") as file:
         for line in file.readlines():
             # remove newline at end
             yield line.rstrip()
 
 
-def yield_chunks(filename: str, delimiter: str):
-    for line in yield_lines(filename):
-        yield line.split(delimiter)
+def yield_chunks(filename: str, current_dir : str, delimiter: str):
+    filename_full_path = os.path.join(current_dir, filename)
+    with open(filename_full_path, "r") as file:
+        return file.read().split(delimiter)
