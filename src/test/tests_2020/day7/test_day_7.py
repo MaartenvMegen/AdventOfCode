a19ccd1f691240771ccba7ac4b22193aa3278f70
Maarten <maarten.vanmegen@nedap.com>
2020-12-07 12:48:39 +0100

    Day 7 - Restructuring

--- src/test/tests_2020/day7/test_day_7.py
+++ src/test/tests_2020/day7/test_day_7.py
@@ -16,25 +16,20 @@ class Bag():
     def add_origin(self, bag):
         self.origins.add(bag)
 
-    def get_origins(self, bags):
+    def get_origins(self):
+        bags = set()
         for origin in self.origins:
             bags.add(origin)
-            bags.update(origin.get_origins(bags))
+            bags.update(origin.get_origins())
         return bags
 
     def get_contents(self, bags):
-        #print(f'Getting contents for bag {self.color}')
-
         contents = defaultdict(int)
         for color, amount in self.bag_spec.items():
-            #print(f'Getting contents for sub bag {color} of amount {amount}')
             contents[color] += int(amount)
             sub_contents = bags[color].get_contents(bags)
-            #print(f'{color} bag contains {sub_contents}')
             for color, spec_amount in sub_contents.items():
                 contents[color] += int(amount) * spec_amount
-        #print(f'{self.color} bag contains {contents}')
-
         return contents
 
 
@@ -51,66 +46,43 @@ def parse_line(line):
     return Bag(bag_color, bag_spec)
 
 
+def get_bags_from_file(filename):
+    bags = dict()
+    for line in lineyielder.yield_lines_fp(filename, THIS_DIR):
+        bag = parse_line(line[:-1])
+        bags[bag.color] = bag
+    return bags
+
+
+def set_origins(bags):
+    [bags[color].add_origin(bag) for bag in bags.values() for color in bag.bag_spec.keys()]
+
+
 class Day7Tester(unittest.TestCase):
 
     def test_part_a_example(self):
-        bags = dict()
-
-        for line in lineyielder.yield_lines_fp("example.txt", THIS_DIR):
-            bag = parse_line(line[:-1])
-            bags[bag.color] = bag
-
-        # set origins
-        for bag in bags.values():
-            for color in bag.bag_spec.keys():
-                if color in bags.keys():
-                    #print(f'adding color {color} to origin {bag.color}')
-                    bags[color].add_origin(bag)
-
-        anwers = len(bags['shiny gold'].get_origins(set()))
+        bags = get_bags_from_file('example.txt')
+        set_origins(bags)
+        anwers = len(bags['shiny gold'].get_origins())
         self.assertEqual(4, anwers)
 
     def test_part_a_input(self):
-        bags = dict()
-
-        for line in lineyielder.yield_lines_fp("input.txt", THIS_DIR):
-            bag = parse_line(line[:-1])
-            bags[bag.color] = bag
-
-        # set origins
-        for bag in bags.values():
-            for color in bag.bag_spec.keys():
-                if color in bags.keys():
-                    bags[color].add_origin(bag)
-
-        anwers = len(bags['shiny gold'].get_origins(set()))
+        bags = get_bags_from_file('input.txt')
+        set_origins(bags)
+        anwers = len(bags['shiny gold'].get_origins())
         self.assertEqual(213, anwers)
 
     def test_part_b_example(self):
-        bags = dict()
-
-        for line in lineyielder.yield_lines_fp("example.txt", THIS_DIR):
-            bag = parse_line(line[:-1])
-            bags[bag.color] = bag
+        bags = get_bags_from_file('example.txt')
         answer = sum(bags['shiny gold'].get_contents(bags).values())
         self.assertEqual(32, answer)
 
     def test_part_b_example_2(self):
-        bags = dict()
-
-        for line in lineyielder.yield_lines_fp("example2.txt", THIS_DIR):
-            bag = parse_line(line[:-1])
-            bags[bag.color] = bag
-
+        bags = get_bags_from_file('example2.txt')
         answer = sum(bags['shiny gold'].get_contents(bags).values())
         self.assertEqual(126, answer)
 
     def test_part_b(self):
-        bags = dict()
-
-        for line in lineyielder.yield_lines_fp("input.txt", THIS_DIR):
-            bag = parse_line(line[:-1])
-            bags[bag.color] = bag
-
+        bags = get_bags_from_file('input.txt')
         answer = sum(bags['shiny gold'].get_contents(bags).values())
         self.assertEqual(38426, answer)
