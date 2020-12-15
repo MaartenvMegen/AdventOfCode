8f1da207078c62afebaaab2a6c0f39b3993bc4f1
Maarten <maarten.vanmegen@nedap.com>
2020-12-15 09:13:28 +0100

    --- Day 15: Rambunctious Recitation ---

--- src/test/tests_2020/day15/test_day15.py
+++ src/test/tests_2020/day15/test_day15.py
@@ -1,8 +1,63 @@
 import os
 import unittest
+from collections import defaultdict
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
+def take_turn(spoken_last, turn, memory):
+    if spoken_last != 0:
+        word = turn - 1 - spoken_last
+    else:
+        word = 0
+
+    last_spoken = memory[word]
+    memory[word] = turn
+
+    return word, last_spoken
+
+
+def play_game(starting_words, until=2020):
+    # Starting phase: Initialize with starting words
+    memory = defaultdict(int)
+    for turn, word in enumerate(starting_words, 1):
+        memory[word] = turn
+
+    # Play game until desired end time
+    prev_word = starting_words[-1]
+    spoken_last = memory[prev_word]
+    for turn in range(len(starting_words) + 1, until + 1):
+        prev_word, spoken_last = take_turn(spoken_last, turn, memory)
+
+    # Once done return the last word that was spoken
+    return prev_word
+
+
 class Day15Tester(unittest.TestCase):
-    def test_example_a(self):
-        pass
+    def test_example_a1(self):
+        prev_word = play_game([0, 3, 6])
+        self.assertEqual(436, prev_word)
+
+    def test_example_a2(self):
+        prev_word = play_game([1, 3, 2])
+        self.assertEqual(1, prev_word)
+
+    def test_example_a3(self):
+        prev_word = play_game([2, 1, 3])
+        self.assertEqual(10, prev_word)
+
+    def test_example_a4(self):
+        prev_word = play_game([1, 2, 3])
+        self.assertEqual(27, prev_word)
+
+    def test_a(self):
+        prev_word = play_game([17, 1, 3, 16, 19, 0])
+        self.assertEqual(694, prev_word)
+
+    def test_example_b1(self):
+        prev_word = play_game([0, 3, 6], until=30000000)
+        self.assertEqual(175594, prev_word)
+
+    def test_b(self):
+        prev_word = play_game([17, 1, 3, 16, 19, 0], until=30000000)
+        self.assertEqual(21768614, prev_word)
