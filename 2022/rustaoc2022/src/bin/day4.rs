1ee3dcca0eb9f8f20832dec092908ad96550642b
Maarten van Megen <maarten.vanmegen@nedap.com>
2022-12-05 17:35:43 +0100

    day 4: Camp Cleanup

--- 2022/rustaoc2022/src/bin/day4.rs
+++ 2022/rustaoc2022/src/bin/day4.rs
@@ -1,38 +1,53 @@
 extern crate core;
 
 use std::collections::HashSet;
+use std::str::FromStr;
 
 fn part1(input: &str) -> u64 {
-    input.trim().split("\n").map( |line | {
-        let (first, second) = line.split_at(line.len()/2);
-        let first : HashSet<char> = HashSet::from_iter(first.chars());
-        let second : HashSet<char> = HashSet::from_iter(second.chars());
-        let char = first.intersection(&second).next().unwrap();
-        match char.is_ascii_lowercase() {
-            true => *char as u64 - 96,
-            false => *char as u64 - 64 + 26,
-        }
-    }).sum()
+    input
+        .trim()
+        .split("\n")
+        .map(|line| {
+            let sector_spec = get_sectors_from_line(line);
+
+            return if sector_spec[0].is_subset(&sector_spec[1]) {
+                1
+            } else if sector_spec[1].is_subset(&sector_spec[0]) {
+                1
+            } else {
+                0
+            };
+        }).sum()
+}
+
+fn get_sectors_from_line(line: &str) -> (HashSet<u64>, HashSet<u64>) {
+    let mut elves = line
+        .split(",")
+        .map(|section| {
+            let range: Vec<u64> = section
+                .split("-")
+                .map(|sector| u64::from_str(sector).unwrap())
+                .collect();
+            HashSet::from_iter(range[0]..range[1] + 1)
+        });
+    (elves.next().unwrap(), elves.next().unwrap())
 }
 
 fn part2(input: &str) -> u64 {
-    let backpacks : Vec<&str>= input.trim().lines().collect();
-    backpacks.windows(3).step_by(3).map( |pair | {
-        let first : HashSet<char> = HashSet::from_iter(pair[0].chars());
-        let second : HashSet<char> = HashSet::from_iter(pair[1].chars());
-        let overlap1 : HashSet<char> = first.intersection(&second).map( |char| *char ).collect();
-        let third : HashSet<char> = HashSet::from_iter(pair[2].chars());
-        let char  = third.intersection(&overlap1).next().unwrap();
-        match char.is_ascii_lowercase() {
-            true => *char as u64 - 96,
-            false => *char as u64 - 64 + 26,
-        }
-    }).sum()
+    input.trim().split("\n").map(|line| {
+        let sector_specs = get_sectors_from_line(line);
+        let overlap = sector_specs[0].intersection(&sector_specs[1]).collect::<Vec<&u64>>();
+        return if overlap.len() > 0 {
+            1
+        } else {
+            0
+        };
+    }).c()
 }
 
 fn main() {
-    let example = include_str!(r"../../resources/day3-example.txt");
-    let input = include_str!(r"../../resources/day3-input.txt");
+    let example = include_str!(r"../../resources/day4-example.txt");
+    let input = include_str!(r"../../resources/day4-input.txt");
 
     rustaoc2022::run_matrix(part1, part2, example, input);
 }
@@ -43,15 +58,15 @@ mod day3 {
 
     #[test]
     fn test_example() {
-        let input = include_str!(r"../../resources/day3-example.txt");
-        assert_eq!(157, part1(input));
-        assert_eq!(70, part2(input));
+        let input = include_str!(r"../../resources/day4-example.txt");
+        assert_eq!(2, part1(input));
+        assert_eq!(4, part2(input));
     }
 
     #[test]
     fn test_input() {
-        let input = include_str!(r"../../resources/day3-input.txt");
-        assert_eq!(7763, part1(input));
-        assert_eq!(2569, part2(input));
+        let input = include_str!(r"../../resources/day4-input.txt");
+        assert_eq!(459, part1(input));
+        assert_eq!(779, part2(input));
     }
 }
