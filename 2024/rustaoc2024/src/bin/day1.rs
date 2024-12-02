7a9c1d0f412dc75ae67e40b522de6a10df1e62e1
Maarten van Megen <maarten.vanmegen@nedap.com>
2024-12-02 17:02:34 +0100

    day1 and day2 2024

---
+++ 2024/rustaoc2024/src/bin/day1.rs
@@ -0,0 +1,66 @@
+use std::collections::HashMap;
+
+fn part1(input: &str) -> u64 {
+    let parts: Vec<&str> = input.trim().split('\n').collect();
+    
+    let mut col1: Vec<i32> = Vec::new();
+    let mut col2: Vec<i32> = Vec::new();
+
+    for line in parts {
+        let line = line;
+        let mut iter = line.split_whitespace();
+        col1.push(iter.next().unwrap().parse().unwrap());
+        col2.push(iter.next().unwrap().parse().unwrap());
+    }
+
+    // Sort both columns independently
+    col1.sort();
+    col2.sort();
+
+    // println!("Sorted Col1: {:?}", col1);
+    // println!("Sorted Col2: {:?}", col2);
+    // Calculate differences and print results
+    let mut dif_total = 0;
+    for (a, b) in col1.iter().zip(col2.iter()) {
+        let diff = a.abs_diff(*b);
+        //println!("Difference: {}", diff);
+        dif_total += diff;
+    }
+
+    return dif_total as u64
+}
+
+fn part2(input: &str) -> u64 {
+    let parts: Vec<&str> = input.trim().split('\n').collect();
+
+    let mut col1: Vec<i32> = Vec::new();
+    let mut col2: Vec<i32> = Vec::new();
+
+    for line in parts {
+        let line = line;
+        let mut iter = line.split_whitespace();
+        col1.push(iter.next().unwrap().parse().unwrap());
+        col2.push(iter.next().unwrap().parse().unwrap());
+    }
+
+    // Sort both columns independently
+    col1.sort();
+    col2.sort();
+
+    // Create a HashMap to store the counts
+    let mut counts: HashMap<i32, usize> = HashMap::new();
+
+    // Iterate over col1 and count occurrences in col2
+    for num in &col1 {
+        let count = col2.iter().filter(|&x| x == num).count();
+        counts.insert(*num, count);
+    }
+
+    col1.iter().fold(0, |acc, x| acc + (x * counts[x] as i32)) as u64
+}
+
+fn main() {
+    let example = include_str!(r"../../resources/day1-example.txt");
+    let input = include_str!(r"../../resources/day1-input.txt");
+    rustaoc2024::run_matrix(part1, part2, example, input);
+}
---
+++ 2024/rustaoc2024/src/bin/day2.rs
@@ -0,0 +1,101 @@
+use std::collections::HashMap;
+#[derive(PartialEq)]
+enum SequenceState {
+    Increasing,
+    Decreasing,
+    Mixed,
+    Equal,
+    ToBig,
+    Start,
+}
+
+fn is_safe_sequence(levels: &[i32]) -> bool {
+    let mut state = SequenceState::Start;
+    let mut prev = levels[0];
+
+
+    for i in 1..levels.len() {
+        let curr = levels[i];
+        let diff = curr - prev;
+
+        state = match diff {
+            0 => SequenceState::Equal,
+            _ if diff.abs() > 3 => SequenceState::ToBig,
+            _ if diff > 0 && state == SequenceState::Decreasing => SequenceState::Mixed,
+            _ if diff < 0 && state == SequenceState::Increasing => SequenceState::Mixed,
+            _ if diff > 0 && state != SequenceState::Decreasing => SequenceState::Increasing,
+            _ if diff < 0 && state != SequenceState::Increasing => SequenceState::Decreasing,
+            _ => SequenceState::Equal,
+        };
+
+        if state == SequenceState::Mixed || state == SequenceState::ToBig || state == SequenceState::Equal {
+            return false;
+
+        }
+
+        prev = curr;
+    }
+
+    true
+}
+
+fn part1(input: &str) -> u64 {
+    let parts: Vec<&str> = input.trim().split('\n').collect();
+    let mut safe_count = 0;
+
+    for line in parts {
+        let mut iter = line.split_whitespace();
+
+        let mut levels: Vec<i32> = Vec::new();
+        while let Some(num_str) = iter.next() {
+            levels.push(num_str.parse::<i32>().unwrap());
+        }
+
+        if is_safe_sequence(&levels) {
+            safe_count += 1;
+        }
+    }
+
+    safe_count
+}
+
+fn part2(input: &str) -> u64 {
+    let parts: Vec<&str> = input.trim().split('\n').collect();
+    let mut safe_count = 0;
+
+    for line in parts {
+        let mut iter = line.split_whitespace();
+
+        let mut levels: Vec<i32> = Vec::new();
+        while let Some(num_str) = iter.next() {
+            levels.push(num_str.parse::<i32>().unwrap());
+        }
+
+        // Check if the report is safe without the Problem Dampener
+        // If not safe, check if it can be made safe by removing one level
+        let mut is_safe = is_safe_sequence(&levels);
+        if !is_safe {
+            for i in 0..levels.len() {
+                let mut modified_levels = levels.clone();
+                modified_levels.remove(i);
+
+                if is_safe_sequence(&modified_levels) {
+                    is_safe = true;
+                    break;
+                }
+            }
+        }
+
+        if is_safe {
+            safe_count += 1;
+        }
+    }
+
+    safe_count
+}
+
+fn main() {
+    let example = include_str!(r"../../resources/day2-example.txt");
+    let input = include_str!(r"../../resources/day2-input.txt");
+    rustaoc2024::run_matrix(part1, part2, example, input);
+}
